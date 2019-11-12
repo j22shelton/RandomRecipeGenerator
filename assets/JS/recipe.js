ab9f14445b7cadd24b808cb39253f6e1dd57ada2
JoJo <shelton.jocelyn@gmail.com>
2019-11-11 21:09:13 -0500

    updated pic, added JS

---
+++ assets/JS/recipe.js
@@ -0,0 +1,7 @@
+var recipes = ['Thai Stir Fry', 'Breaded Garlic Chicken', 'Stuffed Bell Peppers', 'Salmon', 'Korean Crack Slaw', 'Jambalaya', 'Thai Cashew Chicken', 'Red Lentil Curry', 'OnePan Mexican', 'Stri-Fry', 'Chilli', 'Fajitas'
+]
+
+function newRecipe() {
+    var randomNumber = Math.floor(Math.random()*13 = (recipes.length));
+    document.getElementById('recipeDisplay').innerHTLM = recipes[randomNumber];
+}
